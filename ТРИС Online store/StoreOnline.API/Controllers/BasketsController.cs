1b1891797920a8351b049a5206ce8e444cfdcb61
BiologX <74316083+BiologX@users.noreply.github.com>
2025-12-29 12:44:44 +0300

    Add Baskets Controller

---
+++ ТРИС Online store/StoreOnline.API/Controllers/BasketsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreOnline.Core.Abstractions.Services;
+using StoreOnline.Core.Models;
+
+namespace StoreOnline.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketsController : Controller
+    {
+        private readonly IBasketsService basketsService;
+
+        public BasketsController(IBasketsService basketsService)
+        {
+            this.basketsService = basketsService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> AddToBasket([FromBody] Basket request)
+        {
+            request.Id = Guid.NewGuid();
+
+            var response = await basketsService.AddToBasket(request);
+
+            return StatusCode(201, response);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Basket>>> GetAllBasketItems()
+        {
+            var response = await basketsService.GetAllBasketItems();
+
+            return StatusCode(200, response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Guid>> UpdateBasketItem([FromBody] Basket request)
+        {
+            var basket = Basket.Create(
+                request.Id,
+                request.Quantity,
+                request.UserId,
+                request.ProductId
+            );
+
+            var response = await basketsService.UpdateBasketItem(basket);
+
+            return StatusCode(200, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Guid>> RemoveFromBasket(Guid id)
+        {
+            var response = await basketsService.RemoveFromBasket(id);
+
+            return StatusCode(200, response);
+        }
+    }
+}
--- ТРИС Online store/StoreOnline.API/obj/Debug/net9.0/StoreOnline.API.AssemblyInfo.cs
+++ ТРИС Online store/StoreOnline.API/obj/Debug/net9.0/StoreOnline.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642f81cd1067706c9dc378d147839a5c5d2a94f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb31e4895f76bcd47808490fd3f329ed69e9a595")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
