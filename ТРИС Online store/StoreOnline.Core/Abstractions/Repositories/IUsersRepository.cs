aee2c3e3af0d5020f4fb7b421142f465b5846359
BiologX <74316083+BiologX@users.noreply.github.com>
2025-12-29 09:00:18 +0300

    Rename variable in IUsersRepository

--- ТРИС Online store/StoreOnline.Core/Abstractions/Repositories/IUsersRepository.cs
+++ ТРИС Online store/StoreOnline.Core/Abstractions/Repositories/IUsersRepository.cs
@@ -4,7 +4,7 @@ namespace StoreOnline.Core.Abstractions.Repositories
 {
     public interface IUsersRepository
     {
-        Task<Guid> Create(User book);
+        Task<Guid> Create(User user);
         Task<Guid> Delete(Guid id);
         Task<List<User>> Get();
         Task<Guid> Update(Guid id, string name, string login, string passwordHash);
