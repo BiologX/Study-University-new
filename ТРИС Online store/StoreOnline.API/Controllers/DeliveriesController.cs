cb31e4895f76bcd47808490fd3f329ed69e9a595
BiologX <74316083+BiologX@users.noreply.github.com>
2025-12-29 12:41:57 +0300

    Add Delivery Controller

---
+++ ТРИС Online store/StoreOnline.API/Controllers/DeliveriesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreOnline.Core.Abstractions.Services;
+using StoreOnline.Core.Models;
+
+namespace StoreOnline.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeliveriesController : Controller
+    {
+        private readonly IDeliveriesService deliveriesService;
+
+        public DeliveriesController(IDeliveriesService deliveriesService)
+        {
+            this.deliveriesService = deliveriesService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateDelivery([FromBody] Delivery request)
+        {
+            request.Id = Guid.NewGuid();
+
+            var response = await deliveriesService.CreateDelivery(request);
+
+            return StatusCode(201, response);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Delivery>>> GetDeliveries()
+        {
+            var response = await deliveriesService.GetAllDeliveries();
+
+            return StatusCode(200, response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Guid>> UpdateDelivery([FromBody] Delivery request)
+        {
+            var delivery = Delivery.Create(
+                request.Id,
+                request.Address,
+                request.Status,
+                request.CreatedDate,
+                request.OrderId
+            );
+
+            var response = await deliveriesService.UpdateDelivery(delivery);
+
+            return StatusCode(200, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Guid>> DeleteDelivery(Guid id)
+        {
+            var response = await deliveriesService.DeleteDelivery(id);
+
+            return StatusCode(200, response);
+        }
+    }
+}
--- ТРИС Online store/StoreOnline.API/obj/Debug/net9.0/StoreOnline.API.AssemblyInfo.cs
+++ ТРИС Online store/StoreOnline.API/obj/Debug/net9.0/StoreOnline.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bda63cec3f9f6fb31815168f0eadce49a02d790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642f81cd1067706c9dc378d147839a5c5d2a94f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreOnline.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
